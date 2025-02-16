8b806b10e01d708ea89db01ceb7af6f8dd9fab63
chrysanthusobinna <chrysanthusobinna@gmail.com>
2025-02-16 12:14:45 +0000

    Enhanced  registration with reCAPTCHA, honeypot, policy links, and form improvements

--- resources/views/mainsite/pages/serviceusers-registration.blade.php
+++ resources/views/mainsite/pages/serviceusers-registration.blade.php
@@ -4,6 +4,27 @@
 
 @section('header-class', 'header-style-one')
 
+
+@push('scripts')
+	<script>
+		$(document).ready(function() {
+			$('.toggle-all-passwords').click(function() {
+				let passwordFields = $('.password-field'); // Select all password fields
+				let icon = $(this).find('i'); // Select the icon
+				let isPasswordVisible = passwordFields.attr('type') === 'text';
+
+				// Toggle all password fields
+				passwordFields.attr('type', isPasswordVisible ? 'password' : 'text');
+
+				// Change button text and icon based on visibility
+				$(this).html(isPasswordVisible 
+					? '<i class="fa fa-eye"></i> Show Password' 
+					: '<i class="fa fa-eye-slash"></i> Hide Password');
+			});
+		});
+	</script>
+@endpush
+
 @section('content')
 	<!-- Start main-content -->
 	<section class="page-title" style="background-image: url(/mainsite-assets/images/background/page-title.jpg);">
@@ -63,6 +84,9 @@
                                     <label for="first_name" class="text-secondary">First Name</label>
                                     <input type="text" id="first_name" name="first_name" class="form-control" value="{{ old('first_name') }}" required>
                                 </div>
+                                <div class="form-group col-lg-12" style="display:none;">
+                                    @honeypot
+                                </div>
                                 <div class="form-group col-lg-12">
                                     <label for="middle_name" class="text-secondary">Middle Name</label>
                                     <input type="text" id="middle_name" name="middle_name" class="form-control" value="{{ old('middle_name') }}">
@@ -95,22 +119,43 @@
                                     <label for="county" class="text-secondary">County</label>
                                     <input type="text" id="county" name="county" class="form-control" value="{{ old('county') }}" required>
                                 </div>
-                                
                                 <div class="form-group col-lg-12">
                                     <label for="country" class="text-secondary">Country</label>
                                     <select id="country" name="country" class="form-control" required>
                                         @include('partials._select_country')
                                     </select>
                                 </div>
-                            
+                        
                                 <!-- Password Fields -->
                                 <div class="form-group col-lg-6">
                                     <label for="password" class="text-secondary">Password</label>
-                                    <input type="password" id="password" name="password" class="form-control" required>
+                                    <input type="password" id="password" name="password" class="form-control password-field" required>
                                 </div>
                                 <div class="form-group col-lg-6">
                                     <label for="password_confirmation" class="text-secondary">Confirm Password</label>
-                                    <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" required>
+                                    <input type="password" id="password_confirmation" name="password_confirmation" class="form-control password-field" required>
+                                </div>
+
+                                <!-- Show/Hide Password Button -->
+                                <div class="form-group col-lg-12">
+                                    <button class="btn btn-outline-secondary toggle-all-passwords btn-block" style="width:100%" type="button">
+                                        <i class="fa fa-eye"></i> Show Password
+                                    </button>
+                                </div>                        
+                                <!-- reCAPTCHA -->
+                                <div class="form-group col-lg-12 d-flex justify-content-center">
+                                    {!! NoCaptcha::display() !!}
+                                    {!! NoCaptcha::renderJs() !!}
+                                </div>
+
+                                <!-- Terms and Privacy Agreement -->
+                                <div class="form-group col-lg-12 text-center mb-3">
+                                    <p class="text-muted" style="font-size: 14px;">
+                                        By clicking the "Register Now" button, you confirm that you have read and agree to our
+                                        <a href="{{ route('mainsite.terms.serviceuser') }}" target="_balnk"   class="text-secondary" style="text-decoration: underline;">Terms and Conditions</a>
+                                        and our
+                                        <a href="{{ route('mainsite.privacy') }}" target="_balnk"  class="text-secondary" style="text-decoration: underline;">Privacy Policy</a>.
+                                    </p>
                                 </div>
 
                                 <div class="form-group col-lg-12">
@@ -120,7 +165,7 @@
                                 </div>
                             </div>
                         </form>
-
+                        
                                                 
                     </div>
                     <!--End Registration Form -->
